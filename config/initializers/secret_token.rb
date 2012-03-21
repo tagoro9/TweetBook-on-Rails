fddb05f77ef1b6aa45310be686c218f4840acd46
Victor MA <tagoro9@gmail.com>
2012-03-21 12:53:02 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TweetBookOnRails::Application.config.secret_token = 'f03a48918b457319fc57085562233ffe576bfeefac889af3dd4e31ae37345141716e28d4e7878b842de102db9458fe947730fa2c8b4f788896def50417a3c3ff'
