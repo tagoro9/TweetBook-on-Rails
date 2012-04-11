99a6d1de557ab0fd29daecbdf13e97284b7066eb
Victor MA <tagoro9@gmail.com>
2012-04-11 13:45:41 +0100

    Dando estilo al login

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def header
+    if @title == "Inicio"
+      render 'layouts/login_header'
+    else
+      render 'layouts/header'
+    end
+  end
+  
+  def footer
+    if @title == "Inicio"
+      render 'layouts/login_footer'
+    else
+      render 'layouts/footer'
+    end
+  end
 end
