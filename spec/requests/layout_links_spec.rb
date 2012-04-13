44366586628278012bba4c1837ad2b11547806e9
Jose Daniel <jdcuadrado3@gmail.com>
2012-04-13 13:12:50 +0100

    - Añadidas pruebas para el modelo de usuarios

---
+++ spec/requests/layout_links_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe "LinksDePaginas" do    
+    
+  it "Deberia tener una pagina inicial en '/'" do
+    get '/'
+    response.should have_selector('title', :content => "Inicio")
+  end
+  
+  it "deberia tener una pagina home en '/home'" do
+    get '/home'
+    response.should have_selector('title', :content => "Home")
+  end
+  
+  it "deberia tener una pagina de usuarios en '/usuarios'" do
+    get '/users'
+    response.should have_selector('title', :content => "Usuarios")
+  end
+
+  it "Deberia tener una pagina cuenta en '/cuenta'" do
+    get '/profile'
+    response.should have_selector('title', :content => "Cuenta")
+  end
+
+  it "Deberia tener una pagina de preguntas frecuentes '/faq'" do
+    get '/faq'
+    response.should have_selector('title', :content => "FAQ")
+  end
+  
+end
