0e6ce2ae20f323725be06f1dd1b88903b222c46c
llosimura <alfonsoglezluis@gmail.com>
2012-05-09 13:50:37 +0100

    js

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,3 +1,23 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
-$('.dropdown-toggle').dropdown()
+
+$(document).ready(function(){
+	
+	$('.dropdown-toggle').dropdown()
+	
+	$("#micropost_content").on("keyup", function(){
+		valor = 140 - $("#micropost_content").val().length
+		$("#charactersLeft").html(valor)
+		if (($("#micropost_content").val().length) > 140) {
+			$("#micropost_submit").attr('disabled','disabled')
+			
+		}
+		else {
+			$("#micropost_submit").removeAttr('disabled')
+		}
+	})
+})
+
+
+
+
