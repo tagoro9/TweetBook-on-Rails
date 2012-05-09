6baa05528bd443fe11a9358858e37c00fb4f6fc8
Victor MA <tagoro9@gmail.com>
2012-05-09 13:45:40 +0100

    Paginacion arreglada

---
+++ config/initializers/will_paginate.rb
@@ -0,0 +1,4 @@
+WillPaginate::ViewHelpers.pagination_options[:previous_label] = '&lt;&lt; Anterior'
+WillPaginate::ViewHelpers.pagination_options[:next_label] = 'Siguiente &gt;&gt;'
+WillPaginate::ViewHelpers.pagination_options[:outer_window] = 0
+WillPaginate::ViewHelpers.pagination_options[:inner_window] = 1
