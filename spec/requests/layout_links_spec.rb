f03b048cb9bbceafb1959b3d820c5bf12f0bfd63
Victor MA <tagoro9@gmail.com>
2012-05-29 20:28:33 +0100

    Footer y pagina de login

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -8,25 +8,6 @@ describe "LinksDePaginas" do
     response.should have_selector('title', :content => "Inicio")
   end
   
-=begin
-  
-  it "deberia tener una pagina home en '/home'" do
-    get '/home'
-    response.should have_selector('title', :content => "Home")
-  end
-  
-  it "deberia tener una pagina de usuarios en '/usuarios'" do
-    get '/users'
-    response.should have_selector('title', :content => "Usuarios")
-  end
-
-  it "Deberia tener una pagina cuenta en '/cuenta'" do
-    get '/profile'
-    response.should have_selector('title', :content => "Cuenta")
-  end
-  
-=end
-
   it "Deberia tener una pagina de preguntas frecuentes '/faq'" do
     get '/faq'
     response.should have_selector('title', :content => "FAQ")
