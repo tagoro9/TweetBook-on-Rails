01b4d1259d8dfcc4425d9e7620747ebcd8d73336
Victor MA <tagoro9@gmail.com>
2012-05-30 19:25:53 +0100

    Vuelta al antiguo 404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,18 +2,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   include SessionsHelper
-  rescue_from ActionController::RoutingError, :with => :render_404
-  rescue_from ActionController::UnknownAction, :with => :render_404
-  rescue_from ActiveRecord::RecordNotFound, :with => :render_404
-  
-  def render_404
-    if /(jpe?g|png|gif)/i === request.path
-      render :text => "404 Not Found", :status => 404
-    else
-      render :template => "shared/404", :layout => false, :status => 404
-    end
-  end
-  
-  
-  
 end
