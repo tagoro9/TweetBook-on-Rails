19414c6e36b75788c60d05f6daca220b813b2e1f
llosimura <alfonsoglezluis@gmail.com>
2012-06-11 17:24:45 +0100

    Added support for link in microposts

--- app/helpers/microposts_helper.rb
+++ app/helpers/microposts_helper.rb
@@ -4,8 +4,24 @@ module MicropostsHelper
   def wrap(content)
     sanitize(raw(content.split.map{ |s| wrap_long_string(s) }.join(' ')))
   end
+  
+  def any_url (text)
+    content = text.clone
+    params = Hash.new
+    url = content.scan(/(?:http:\/\/)?\www\.\w+.\S+/)
+    url.each do |a|
+      http = a.sub(/http:\/\//,"")
+      link = "<a href=\"http:\/\/#{http}\"> #{http} </a>"
+      content.sub!(a,link)    
+      (params[:video]  = "http:\/\/"+http) if (http.match(/www.youtube.com/)) != nil
+      (params[:imagen] = "http:\/\/"+http) if (http.match(/\www\.\w+.\S+.jpe?g/)) != nil
+    end
+    [content,params]
+  end
 
   private
+  
+    
 
     def wrap_long_string(text, max_width = 30)
       zero_width_space = "&#8203;"
