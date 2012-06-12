e54fdfd317f369d9307814fd86e0cd56f0ddbad8
Victor MA <tagoro9@gmail.com>
2012-06-12 15:42:59 +0100

    Similares a ti

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,7 +26,7 @@ class ApplicationController < ActionController::Base
   include MicropostsHelper
   
   #unless Rails.application.config.consider_all_requests_local
-    rescue_from Exception, with: :render_500
+    #rescue_from Exception, with: :render_500
     rescue_from ActionController::RoutingError, with: :render_404
     rescue_from ActionController::UnknownController, with: :render_404
     rescue_from ActionController::UnknownAction, with: :render_404
