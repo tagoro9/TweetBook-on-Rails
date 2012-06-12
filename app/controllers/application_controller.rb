a6de4d3e835424019fd903e6ab785dc25c4b88d7
Victor MA <tagoro9@gmail.com>
2012-06-12 12:15:13 +0100

    Imagenes de perfil y nuevo logo login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 # -*- encoding : utf-8 -*-
 
 =begin
+ TODO Hover sobre esquinita microposts!
  TODO pagina usuarios quitar lalalalalala
  TODO estilos al mostrar los usuarios
  TODO al borrar un tweet si es desde el home, redirigirlo al home
